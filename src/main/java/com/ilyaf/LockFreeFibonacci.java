81c28fb988056b9d4e90ce73cc9b76eafb043b34
Ilya Frolov <fiss205@gmail.com>
2017-10-21 16:17:46 +0300

    Initial commit.

---
+++ src/main/java/com/ilyaf/LockFreeFibonacci.java
@@ -0,0 +1,66 @@
+package com.ilyaf;
+
+import net.jcip.annotations.Immutable;
+import net.jcip.annotations.ThreadSafe;
+
+import java.math.BigInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Lock-free thread safe implementation of Fibonacci number generator
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Fibonacci_number">Fibonacci number</a>
+ */
+@ThreadSafe
+public class LockFreeFibonacci {
+
+    /**
+     * Immutable structure to save state for next numbers generation
+     */
+    @Immutable
+    private class State {
+        private BigInteger current;
+        private BigInteger prev;
+
+        State(BigInteger current, BigInteger prev) {
+            this.current = current;
+            this.prev = prev;
+        }
+
+        BigInteger getCurrent() {
+            return current;
+        }
+
+        BigInteger getPrev() {
+            return prev;
+        }
+    }
+
+    private AtomicReference<State> state = new AtomicReference<>(
+            new State(BigInteger.valueOf(1), BigInteger.valueOf(-1))
+    );
+
+    /**
+     * Produces next Fibonacci number without locking.
+     * Sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+     *
+     * @return next Fibonacci number in a sequence
+     */
+    public BigInteger next() {
+        while(true) {
+            State currentState = state.get();
+            BigInteger prev = currentState.getPrev();
+            BigInteger current = currentState.getCurrent();
+
+            // check that the state hasn't been changed to this moment
+            if(currentState == state.get()) {
+                BigInteger nextNumber = prev.add(current);
+
+                // using atomic compareAndSet method
+                if (state.compareAndSet(currentState, new State(nextNumber, current)))
+                    return nextNumber;
+            }
+        }
+    }
+
+}
---
+++ src/test/java/com/ilyaf/LockFreeFibonacciTest.java
@@ -0,0 +1,36 @@
+package com.ilyaf;
+
+import com.devexperts.dxlab.lincheck.LinChecker;
+import com.devexperts.dxlab.lincheck.annotations.Operation;
+import com.devexperts.dxlab.lincheck.annotations.Reset;
+import com.devexperts.dxlab.lincheck.stress.StressCTest;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+/**
+ * Testing Lock-free Fibonacci number generator using linearization
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Linearizability">Linearizability</a>
+ */
+@StressCTest(iterations = 300, actorsPerThread = {"1:3", "1:3", "1:3"})
+public class LockFreeFibonacciTest {
+
+    private LockFreeFibonacci lockFreeFibonacci;
+
+    @Reset
+    public void reset() {
+        lockFreeFibonacci = new LockFreeFibonacci();
+    }
+
+    @Operation
+    public BigInteger next() {
+        return lockFreeFibonacci.next();
+    }
+
+    @Test
+    public void test() {
+        LinChecker.check(LockFreeFibonacciTest.class);
+    }
+
+}
